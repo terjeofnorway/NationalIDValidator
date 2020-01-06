ab874c3951894e5cb7201aa3d094eab50a1f76c6
Terje Karlsen <terjeofnorway@gmail.com>
2020-01-06 20:25:48 +0100

    Added first version of fnr validation

--- src/validation.ts
+++ src/validation.ts
@@ -1,10 +1,42 @@
 import { IValidation } from './validation.interface';
 
+type CalculateControl = (c: Controls) => number;
+
+enum Controls {
+  CONTROL1,
+  CONTROL2,
+}
+
 class Validation implements IValidation {
   constructor(private id: string) {};
 
+  controlMultipliers: {[key in Controls]: Array<number>} = {
+    [Controls.CONTROL1]: [3,7,6,1,8,9,4,5,2],
+    [Controls.CONTROL2]: [5,4,3,2,7,6,5,4,3,2],
+  }
+
+  private calculateControl: CalculateControl = (c: Controls) => {
+    const multipliers: Array<number> = this.controlMultipliers[c];
+
+    let sum = 0;
+    for(let i = 0 ; i < multipliers.length ; i++) {
+      const digit = parseInt(this.id[i], 10);
+      sum += digit * multipliers[i];
+    }
+
+    return 11 - (sum % 11);
+  }
+
   checkValidity = () => {
-    return this.id.length > 0;
+    if (!this.id) { throw new Error('ID cannot be null, empty string or undefined.')}
+    if (this.id.length === 0) { return false;}
+
+    const c1: number = this.calculateControl(Controls.CONTROL1);
+    const c2: number = this.calculateControl(Controls.CONTROL2);
+    const k1: number = parseInt(this.id[9], 10);
+    const k2: number = parseInt(this.id[10], 10);
+
+    return c1 === k1 && c2 === k2;
   }
 }
 
--- tests/validation.test.ts
+++ tests/validation.test.ts
@@ -1,9 +1,27 @@
 import { Validation } from "../src/Validation";
 
 describe('Test Validation class', () => {
-  test('returns false if value is empty', () => {
-    const mockID = '';
-    const validation = new Validation(mockID);
+  test('it correctly validates a valid fnr', () => {
+    const mockFnr: string = '06010732393';
+    const validation: Validation = new Validation(mockFnr);
+    expect(validation.checkValidity()).toBe(true);
+  })
+
+  test('it correctly invalidates an invalid fnr', () => {
+    const mockFnr: string = '12345678912';
+    const validation: Validation = new Validation(mockFnr);
     expect(validation.checkValidity()).toBe(false);
+  })
+
+  test('it correctly throws an error when fnr is null', () => {
+    const mockFnr: any = null;
+    const validation: Validation = new Validation(mockFnr);
+    expect(validation.checkValidity).toThrow();
+  })
+
+  test('it correctly throws an error when fnr is empty', () => {
+    const mockFnr: any = '';
+    const validation: Validation = new Validation(mockFnr);
+    expect(validation.checkValidity).toThrow();
   })
 });
