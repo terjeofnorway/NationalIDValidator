f6aa62fe7d0f908853f5ba1f1879f75ceb1a145e
Terje Karlsen <terjeofnorway@gmail.com>
2020-01-06 18:01:37 +0100

    Threw out Chai / Mocha. Added Jest.

--- tests/validation.test.ts
+++ tests/validation.test.ts
@@ -1,11 +1,9 @@
 import { Validation } from "../src/Validation";
 
-var expect = require('chai').expect;
-
 describe('Test Validation class', () => {
-  it('returns false if value is empty', () => {
+  test('returns false if value is empty', () => {
     const mockID = '';
     const validation = new Validation(mockID);
-    expect(validation.checkValidity()).to.equal(false);
+    expect(validation.checkValidity()).toBe(false);
   })
 });
