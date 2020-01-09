ab313c45db43244aca16f275b5f887f2bc1958f3
Terje Karlsen <terjeofnorway@gmail.com>
2020-01-09 20:34:11 +0100

    Updated package and test fixes

--- tests/validation.test.ts
+++ tests/validation.test.ts
@@ -1,33 +1,35 @@
-import { Validation } from "../src/Validation";
+import { Validation } from '../src/Validation';
 
 describe('Test Validation class', () => {
   test('it correctly validates a valid fnr', () => {
-    const mockFnr: string = '06010732393';
+    const mockFnr = '06010732393';
     const validation: Validation = new Validation(mockFnr);
     expect(validation.checkValidity()).toBe(true);
-  })
+  });
 
   test('it correctly invalidates an invalid fnr', () => {
-    const mockFnr: string = '12345678912';
+    const mockFnr = '12345678912';
     const validation: Validation = new Validation(mockFnr);
     expect(validation.checkValidity()).toBe(false);
-  })
+  });
 
   test('it correctly throws an error when fnr is null', () => {
+    /* eslint-disable-next-line */
     const mockFnr: any = null;
     const validation: Validation = new Validation(mockFnr);
     expect(validation.checkValidity).toThrow();
-  })
+  });
 
   test('it correctly throws an error when fnr is undefined', () => {
+    /* eslint-disable-next-line */
     const mockFnr: any = undefined;
     const validation: Validation = new Validation(mockFnr);
     expect(validation.checkValidity).toThrow();
-  })
+  });
 
   test('it correctly throws an error when fnr is empty', () => {
-    const mockFnr: any = '';
+    const mockFnr = '';
     const validation: Validation = new Validation(mockFnr);
     expect(validation.checkValidity).toThrow();
-  })
+  });
 });
