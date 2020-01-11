5d937fc68b834c3cf6d4f315ccae750ed87ba033
Terje Karlsen <terjeofnorway@gmail.com>
2020-01-11 20:01:48 +0100

    Added webpack bundling and minification.

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+/* eslint-disable */
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.ts',
+  devtool: 'inline-source-map',
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist'),
+    library: '',
+    libraryTarget: 'commonjs'
+  },
+};
